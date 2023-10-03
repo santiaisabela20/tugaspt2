b6c500fd8609e8b87b7578ccf6dca8ab0985dc49
santiaisabela20 <117247746+santiaisabela20@users.noreply.github.com>
2023-10-03 23:04:21 +0700

    Add files via upload

---
+++ santia_tugas2.js
@@ -0,0 +1,9 @@
+
+const firstName = "santia";
+const lastName = "isabela";
+const age = 20; 
+const  isMarried = false; 
+
+console.log(`Nama saya adalah ${firstName} ${lastName}`);
+console.log(`Umur saya adalah ${age} tahun`);
+console.log(`Status menikah saya ${isMarried ? 'sudah' : 'belum'} menikah`);
